ffe75f0cf58ac91b41caab66dec943846c434bdc
Prince Pal <princepal262501@gmail.com>
2025-12-08 13:06:14 +0530

    added Sorting

--- Searching/Searching.java
+++ Searching/Searching.java
@@ -11,28 +11,31 @@ class Searching {
 
     }
 
-
-    public static int binarySearch(int arr[], int key) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start < end) {
+    public static int binarySearch(int arr[], int key){
+        int start = 0 , end = arr.length-1;
+        while(start<=end){
             int mid = (start + end) / 2;
-            if (arr[mid] == key) {
+            if(arr[mid] == key){
                 return mid;
-            } else if (arr[mid] < key) {
+            }
+            else if(key > arr[mid]){
                 start = mid + 1;
-
-            } else {
+            }else{
                 end = mid - 1;
             }
+            
+
         }
         return -1;
-
     }
+
+
+   
+    
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
-        int key = 4;
+        int key = 1;
         //    int index =  linearSearch(arr,key);
         int index = binarySearch(arr, key);
 
@@ -44,3 +47,4 @@ class Searching {
 
     }
 }
+
