44510e84568b4bfcdca31fe6036dd1b2551eb3e7
Prince Pal <princepal262501@gmail.com>
2025-12-30 21:41:28 +0530

    fix some bug

--- 09_2D_Arrays/SearchElement.java
+++ 09_2D_Arrays/SearchElement.java
@@ -6,7 +6,7 @@ class SearchElement {
         int m = arr[0].length;
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 if (arr[i][j] == key) {
                     System.out.println("Key found at " + "(" + i + ","+ j +")");
                     return true;
