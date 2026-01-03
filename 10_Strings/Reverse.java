f06d2fc4f0c37986527ba226a4862c363d3be0ca
Prince Pal <princepal262501@gmail.com>
2026-01-03 14:42:53 +0530

    Add Reverse Function using Two Pointer Approach

--- 10_Strings/Reverse.java
+++ 10_Strings/Reverse.java
@@ -13,9 +13,29 @@ class Reverse {
 
     }
 
+    public static String reverseByTwoPointer(String str){
+         char ch[] = str.toCharArray();
+
+         int left = 0, right = ch.length - 1;
+
+         while(left < right){
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+
+            left ++;
+            right --;
+         }
+
+         return new String(ch);
+
+         
+    }
+
     public static void main(String[] args) {
         String str = "Prince";
-        String reverse = ReverseStr(str);
+        // String reverse = ReverseStr(str);
+        String reverse = reverseByTwoPointer(str);
         System.out.print(reverse);
     }
 }
