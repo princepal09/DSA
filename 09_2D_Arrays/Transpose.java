cc6766570e35b5ae23f897df33566454db56e539
Prince Pal <princepal262501@gmail.com>
2026-02-27 15:13:26 +0530

    one qs added that is what if input is always a square matrix so Time will be O(n*m) and Space will be O(1)

--- 09_2D_Arrays/Transpose.java
+++ 09_2D_Arrays/Transpose.java
@@ -16,6 +16,7 @@ public class Transpose {
         return result;
     }
 
+    // What if the input is always a sqaure matrix
     public static void printArr(int arr[][]) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
@@ -25,14 +26,28 @@ public class Transpose {
         }
     }
 
-    public static void main(String[] args) {
-        int arr[][] = {{2, 4, 6, 2}, {1, 3, 6, 8}, {9, 4, 1, 5}};
-        System.out.println("Before Transpose the Matrix");
-        printArr(arr);
-        int res[][] = transposeMat(arr);
-        System.out.println("After Transpose the Matrix");
+    public static void SQMat(int mat[][]) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = i + 1; j < mat.length; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+    }
 
-        printArr(res);
+    public static void main(String[] args) {
+        // int arr[][] = {{2, 4, 6, 2}, {1, 3, 6, 8}, {9, 4, 1, 5}};
+        // System.out.println("Before Transpose the Matrix");
+        // printArr(arr);
+        // int res[][] = transposeMat(arr);
+        // System.out.println("After Transpose the Matrix");
+
+        // printArr(res);
+        int sqaureMat[][] = {{2, 4, 1}, {-10, 5, -11}, {18, -7, 6}};
+
+        SQMat(sqaureMat);
+        printArr(sqaureMat);
 
     }
 }
