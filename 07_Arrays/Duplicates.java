560ad5cb1c73d467e43553a9069b3a673727d7aa
Prince Pal <princepal262501@gmail.com>
2026-03-04 22:34:26 +0530

    return the unique element

--- 07_Arrays/Duplicates.java
+++ 07_Arrays/Duplicates.java
@@ -1,22 +1,26 @@
-public class Duplicates{
-    
-    public static int removeDup(int arr[]){
+public class Duplicates {
+
+    public static int removeDup(int arr[]) {
         int n = arr.length;
         int x = 0;
-        for(int i = 1; i < n ; i++){
-           if(arr[i] != arr[x]){
-            x += 1;
-            arr[x] = arr[i];
-           }
+
+        for(int i = 1; i < n; i++){
+            if(arr[i] != arr[x]){
+                x += 1;
+                arr[x] = arr[i];
+            }
         }
         return x + 1;
     }
+
     public static void main(String args[]){
         int [] arr = {5,5,7,8,8,9,9,9,10,10};
         int ans = removeDup(arr);
-        System.out.println(ans);
-        for(int nums : arr){
-            System.out.print(nums);
+
+        System.out.println("Unique count: " + ans);
+
+        for(int i = 0; i < ans; i++){
+            System.out.print(arr[i] + " ");
         }
     }
 }
