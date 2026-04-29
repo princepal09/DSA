8d80fe20738a54473ff1b4455f1117ddbb973646
Prince Pal <princepal262501@gmail.com>
2026-04-29 22:28:10 +0530

    added linkedList

---
+++ 15_LinkedList/Main.java
@@ -0,0 +1,33 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        System.out.print(list);
+
+        list.set(2, 10);
+        list.get(1);
+
+        System.out.println("Size of list is " + list.size());
+        System.out.print(list.isEmpty());
+
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        list.clear();
+        // list.toArray();
+        list.contains(40);
+
+    }
+}
