b3aa686298e793ccccd71a1315987c475fdabc49
Prince Pal <princepal262501@gmail.com>
2026-04-30 23:52:11 +0530

    learned common methods of stack

---
+++ 16_Stack/Main.java
@@ -0,0 +1,27 @@
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+
+        System.out.println(st);
+
+        st.pop();
+        System.out.println(st);
+
+        System.out.println(st.peek());
+        System.out.println(st.search(30));
+        System.out.println(st.empty());
+
+
+
+
+
+    }
+}
