268e4c8b0768b0b418bedcd58307e68fb5d9feca
Prince Pal <princepal262501@gmail.com>
2026-05-02 11:54:43 +0530

    added queue

---
+++ 17_Queue/Main.java
@@ -0,0 +1,27 @@
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        // Queue<Integer> q = new ArrayDeque<>();
+        Queue<Integer> q1 = new PriorityQueue<>();
+
+        // q.offer(10);
+        // q.offer(20);
+        // q.offer(30);
+        // q.offer(40);
+        // System.out.println(q);
+
+        // System.out.println(q.peek());
+
+        // System.out.println(q.poll());
+
+        // System.out.println(q);
+
+    }
+
+}
