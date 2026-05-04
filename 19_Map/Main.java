819da47c87ab801ddcbc3d9cc9660570573afa3b
Prince Pal <princepal262501@gmail.com>
2026-05-04 23:38:45 +0530

    learnt Map

--- 19_Map/Main.java
+++ 19_Map/Main.java
@@ -1,6 +1,8 @@
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
@@ -16,5 +18,34 @@ public class Main {
         table.put("chi", "China");
         table.putAll(mapping);
         System.out.println(table);
+
+        // Deletion 
+        table.remove("en");
+
+        table.size();
+
+        // table.clear();
+        table.putIfAbsent("in", "india2");
+
+        table.get("chi");
+
+        table.containsKey("us");
+
+        table.containsValue("India");
+
+        table.replace("in", "indonesia");
+
+        Set<String> st = table.keySet();
+
+        Collection<String> st1 = table.values();
+
+        // get all entries from set
+// get all entries from map
+        Set<Map.Entry<String, String>> entries = table.entrySet();
+
+        for (Map.Entry<String, String> entry : table.entrySet()) {
+            System.out.println(entry.getKey() + " value " + entry.getValue());
+        }
+
     }
 }
