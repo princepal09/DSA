952ea529e6c9adbf05e06fe857025cea0f5e4868
Prince Pal <princepal262501@gmail.com>
2026-05-20 23:19:19 +0530

    solved peakindexmountain qs

---
+++ 14_BinarySearch/PeakIndexMountain.java
@@ -0,0 +1,26 @@
+
+public class PeakIndexMountain {
+
+    public static int peakIndex(int arr[]) {
+        int n = arr.length;
+        int s = 0, e = n - 1;
+        int ans = -1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                s = mid + 1;
+            } else {
+                ans = mid;
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 3, 5, 4, 2};
+        int ans = peakIndex(arr);
+        System.out.println(ans);
+    }
+}
