7211dc9b28e921638b7bdb2fe6d30cccd8d68045
Prince Pal <princepal262501@gmail.com>
2026-05-20 23:22:54 +0530

    solved Pivot Index in sorted rotated array

---
+++ 14_BinarySearch/PivotIndex.java
@@ -0,0 +1,30 @@
+
+public class PivotIndex {
+
+    public static int pivotIdx(int arr[]) {
+        int n = arr.length;
+        int s = 0, e = n - 1;
+        int ans = -1;
+
+        if(arr[s] < arr[e]){
+            return -1;
+        }
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] <= arr[n - 1]) {
+                e = mid - 1;
+            } else {
+                ans = mid;
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {50,60,70,10,20,30,40};
+        int ans = pivotIdx(arr);
+        System.out.println(ans);
+    }
+}
