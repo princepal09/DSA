edf1c1bfc0933f32c739071bdcab3f19a3776552
Prince Pal <princepal262501@gmail.com>
2026-05-30 21:28:56 +0530

    added input

--- 01_basics/Input.java
+++ 01_basics/Input.java
@@ -1,7 +1,9 @@
+
 import java.util.*;
 
-public class Input{
-    public static void main(String args[]){
+public class Input {
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the inputL:");
         String input = sc.nextLine();
@@ -9,3 +11,27 @@ public class Input{
     }
 
 }
+
+// import java.util.*;
+// import java.io.*;
+// class Main {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// input a string
+//         String name = br.readLine();
+//         System.out.println(name);
+// input a number
+//    int number = Integer.parseInt(br.readLine());
+//    System.out.println(number);
+// float 
+// float f = Float.parseFloat(br.readLine());
+// System.out.println(f);
+// float 
+// double d = Double.parseDouble(br.readLine());
+// System.out.println(d);
+// boolean
+
+// boolean b = Boolean.parseBoolean(br.readLine());
+// System.out.println(b);
+//     }
+// }
