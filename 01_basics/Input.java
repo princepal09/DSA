f37972c46640fb3da889c201a563cd35b87582d4
Prince Pal <princepal262501@gmail.com>
2026-05-30 22:00:13 +0530

    added sum

--- 01_basics/Input.java
+++ 01_basics/Input.java
@@ -4,10 +4,31 @@ import java.util.*;
 public class Input {
 
     public static void main(String args[]) {
+
+        //whenever --> primitive datatype --> sc.nextLine()
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the inputL:");
+
+        float x = sc.nextFloat();
+        System.out.println(x);
+
+        sc.nextLine();
         String input = sc.nextLine();
         System.out.println(input);
+
+        double y = sc.nextDouble();
+        System.out.println(y);
+
+        char c = sc.next().charAt(0);
+        System.out.println(c);
+
+        sc.close();
+
+        double pi = Math.PI;
+        System.out.println(pi);
+
+        System.out.printf("%2f",pi); // 3.14
+        
     }
 
 }
@@ -30,7 +51,6 @@ public class Input {
 // double d = Double.parseDouble(br.readLine());
 // System.out.println(d);
 // boolean
-
 // boolean b = Boolean.parseBoolean(br.readLine());
 // System.out.println(b);
 //     }
--- 01_basics/SumTwoNum.java
+++ 01_basics/SumTwoNum.java
@@ -1,14 +1,40 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class SumTwoNum {
+
+   public static int sumOfNum(int n){
+      int sum = 0;
+      while(n > 0){
+         int rem = n % 10;
+         sum += rem;
+         n /= 10;
+      }
+      return sum;
+   }
    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the first number:");
-       int a = sc.nextInt();
-       System.out.println("Enter the second number:");
-       int b = sc.nextInt();
-       int sum = a + b;
-      System.out.println("The sum of " + a + " and " + b + " is " + sum);
+
+      // static class can call without a creating an object 
+      // but if your class is not static then you have to create an object of class and then call the fn by the class object
+
+
+      //  Scanner sc = new Scanner(System.in);
+      //  System.out.println("Enter the first number:");
+      //  int a = sc.nextInt();
+      //  System.out.println("Enter the second number:");
+      //  int b = sc.nextInt();
+      //  int sum = a + b;
+      // System.out.println("The sum of " + a + " and " + b + " is " + sum);
+
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+
+      int sum = sumOfNum(n);
+      System.out.println(sum);
+
+
+      sc.close();
+
+
 
    }
 }
