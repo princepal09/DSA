d15b28479ec4fc7478df36f746220f0566979720
Prince Pal <princepal262501@gmail.com>
2026-06-28 20:11:13 +0530

    Update sumTwo.java

--- 03_Functions/sumTwo.java
+++ 03_Functions/sumTwo.java
@@ -3,7 +3,7 @@ public class sumTwo {
     public static void sumTwoNum(int a , int b){
         int sum = a + b;
         System.out.println(sum);
-    }
+    } 
 
     public static void main(String[] args) {
         int a = 2 , b = 5;
