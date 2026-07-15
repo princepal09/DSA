6a16bcb191c256b34a1b5bbeda381670cbd11028
Prince Pal <princepal262501@gmail.com>
2026-07-15 13:48:05 +0530

    add pascal tra qs

---
+++ 13_ArrayList/PascalTr.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class PascalTr {
+
+    public static  List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (numRows == 0) {
+            return res;
+        }
+
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        res.add(firstRow);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> curr = new ArrayList<>();
+            curr.add(1);
+
+            for (int j = 1; j < i; j++) {
+                curr.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+            }
+
+            curr.add(1);
+            res.add(curr);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        List<List<Integer>> ans = generate(n);
+        System.out.print(ans);
+    }
+}
