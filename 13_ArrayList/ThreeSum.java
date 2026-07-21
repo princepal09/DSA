4a29bb369d3ba890354362bd4a203d1000a2cb69
Prince Pal <princepal262501@gmail.com>
2026-07-21 23:59:01 +0530

    solve three sum qs

--- 13_ArrayList/ThreeSum.java
+++ 13_ArrayList/ThreeSum.java
@@ -1,14 +1,18 @@
-        class ThreeSum {
-            public List<List<Integer>> threeSum(int[] nums) {
+
+import java.util.*;
+
+class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         HashSet<List<Integer>> hs = new HashSet<>();
         Arrays.sort(nums);
         int n = nums.length;
-        for(int i = 0; i < n - 2; i++ ){
+        for (int i = 0; i < n - 2; i++) {
             int s = i + 1, e = n - 1;
-            while(s < e){
+            while (s < e) {
                 int sum = nums[i] + nums[s] + nums[e];
-                if(sum == 0){
+                if (sum == 0) {
                     List<Integer> triplets = new ArrayList<>();
                     triplets.add(nums[i]);
                     triplets.add(nums[s]);
@@ -17,16 +21,16 @@
 
                     s++;
                     e--;
-                }else if(sum < 0){
-                    s ++;
-                }else{
-                    e --;
+                } else if (sum < 0) {
+                    s++;
+                } else {
+                    e--;
                 }
             }
         }
 
-       result.addAll(hs);
-       return result;
+        result.addAll(hs);
+        return result;
 
     }
 }
