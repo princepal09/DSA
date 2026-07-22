219a60a2b264c496ee3e6e0ee14366d9f5c1c826
Prince Pal <princepal262501@gmail.com>
2026-07-22 23:56:33 +0530

    Update Main.java

--- 15_LinkedList/Main.java
+++ 15_LinkedList/Main.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Main {
+public class Main { 
 
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>();
