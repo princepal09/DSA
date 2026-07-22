b290b606df1542a1ffceb09890843eafa06ccc0b
Prince Pal <princepal262501@gmail.com>
2026-07-22 23:57:08 +0530

    Update Main.java

--- 15_LinkedList/Main.java
+++ 15_LinkedList/Main.java
@@ -9,7 +9,7 @@ public class Main {
         List<Integer> list = new LinkedList<>();
         list.add(10);
         list.add(20);
-        list.add(30);
+        list.add(30); 
         list.add(40);
 
         System.out.print(list);
