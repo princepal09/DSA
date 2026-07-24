c6853e5b2e39f2eb9abcfae6ca47b875a932f637
Prince Pal <princepal262501@gmail.com>
2026-07-24 20:28:18 +0530

    solve houseRobber problem

---
+++ 12_Recursion/HouseRobber.java
@@ -0,0 +1,22 @@
+class HouseRobber {
+    public static int ans (int nums[], int i){
+        if(i >= nums.length){
+            return 0;
+        }
+
+        //includes
+        int includeAns = nums[i] + ans(nums, i + 2);
+
+        //excludes
+        int excludeAns = 0 + ans(nums, i + 1);
+
+        int finalAns = Math.max(includeAns, excludeAns);
+        return finalAns;
+    }
+    public static void main(String [] args) {
+        int i = 0;
+        int nums[] = {2,7,9,3,1};
+        int ans = ans(nums, i)
+        System.out.println(ans);
+    }
+}
